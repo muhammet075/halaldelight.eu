03df8606c6789dc8c799d5995bf4aecf0a22c7c0
muhammet075 <muhammet075@icloud.com>
2023-12-03 07:36:48 +0100

    changin seo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,11 +9,12 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
         <meta name="format-detection" content="telephone=no"/>
-        <meta name="description" content="Taxi T.C."/>
-        <meta property="og:title" content="Taxi T.C." />
-        <meta name="keywords" content="Taxi T.C."/>
+        <meta name="description" content="Halal Delight EU"/>
+        <meta property="og:title" content="Halal Delight EU" />
+        <meta name="keywords" content="Halal Delight EU"/>
         <meta property="og:url" content="https://www.halaldelight.eu/"/>
         <meta property="og:image" content="/oglogo.png"/>
+        <link rel="icon" type="image/x-icon" href="/logo.png"/>
       </Head>
       <Header/>
       <main>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,12 +7,12 @@ export default function Document() {
           <title>Halal Delight</title>
           <meta name="format-detection" content="telephone=no"/>
           <meta name="viewport" content="width=device-width, user-scalable=no"/>
-          <meta name="description" content="Taxi T.C. brengt u met een luxe en comfort naar uw bestemming. Bezoek de website en maak direct een afspraak."/>
-          <meta name="keywords" content="T.C. TC Taxi"/>
-          <meta name="og:description" content="Taxi T.C. brengt u met een luxe en comfort naar uw bestemming. Bezoek de website en maak direct een afspraak."/>
+          <meta name="description" content="Halal Delight EU"/>
+          <meta name="keywords" content="Halal Delight EU"/>
+          <meta name="og:description" content="Halal Delight EU"/>
           <meta property="og:url" content="https://www.halaldelight.eu/"/>
           <meta property="og:image" content="/logo.png" />
-          <meta property="og:title" content="T.C. Taxi"/>
+          <meta property="og:title" content="Halal Delight EU"/>
       </Head>
       <body>
         <Main />
