19560d468bfaad0b6fa55695a79286c73ebe575d
muhammet075 <muhammet075@icloud.com>
2023-12-04 02:02:27 +0100

    working on site

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,16 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-02BN25BL8M"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-02BN25BL8M')`
+        }}
+      />
       </Head>
       <body>
         <Main />
