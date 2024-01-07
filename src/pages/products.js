2dcf1af22dec5fdac014adca4e9ba834473450e6
muhammet075 <muhammet075@icloud.com>
2024-01-07 04:25:54 +0100

    chagning logos and adding products page

---
+++ src/pages/products.js
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import Head from "next/head";
+
+export default function TermsConditions() {
+  useEffect(() => {
+  });
+
+  return (
+    <>
+      <Head>
+          <title>Halal Delight EU - Products</title>
+          <meta name="description" content="Ingredients and nutritional values products page."/>
+          <meta name="og:description" content="Ingredients and nutritional values products page"/>
+          <meta property="og:url" content="https://www.halaldelight.eu/products"/>
+          <meta property="og:title" content="Halal Delight EU"/>
+      </Head>
+
+      <div className="content">
+        <div>
+            <h1>Products</h1>
+
+            <b>Under Construction...</b>
+            <br/><br/><br/>
+
+        </div>
+      </div>
+    </>
+  );
+}
